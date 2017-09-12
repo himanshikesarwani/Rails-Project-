7d98df09f7df4dfad538b36d867ae2c42a10f6bb
himanshi <himanshikesarwani@gmail.com>
2017-09-12 16:22:28 +0530

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
-class User < ApplicationRecord
+class User < ApplicationRecord 
+  validates :username, presence: true, 
+             uniqueness: { case_sensitive: false },
+             length: { minimum: 3, maximum: 50 }
+   
+  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i 
+  validates :email, presence: true, 
+             uniqueness: { case_sensitive: false },
+             format: { with:  VALID_EMAIL_REGEX }        
   has_many :comments
 end
